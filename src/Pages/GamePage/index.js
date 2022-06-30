e36f49416752557e5d8a02deb4b9af22356ea740
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2022-06-30 19:50:25 +0200

    ✨Added Home and Game Page

---
+++ src/Pages/GamePage/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import "./style.scss";
+
+function GamePage() {
+  return <div>Game</div>;
+}
+
+export default GamePage;
---
+++ src/Pages/HomePage/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import "./style.scss";
+
+function HomePage() {
+  return <div>Home</div>;
+}
+
+export default HomePage;
