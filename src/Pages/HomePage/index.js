1ee633db7bb6d680f058b654d13e59b012efa495
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2022-07-01 18:47:53 +0200

    💄Added new HomePage style

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,8 +1,21 @@
 import React from "react";
-// import "./style.scss";
+import { Link } from "react-router-dom";
+import "./style.scss";
 
 function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div className="HomePage">
+      <img
+        className="logo"
+        src="https://fontmeme.com/permalink/220701/a0e7f0cddbf4e60cda85b36632fd4af2.png"
+        alt="pokemon-schriftart"
+        border="0"
+      />
+      <Link className="btn" to="/game">
+        Start
+      </Link>
+    </div>
+  );
 }
 
 export default HomePage;
